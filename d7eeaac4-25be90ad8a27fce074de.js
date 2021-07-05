0abc38cc76c95c91241b058b60add539bc926765
Dominika Świeboda <dominika_swieboda@outlook.com>
2021-07-05 18:34:05 +0200

    Updates

---
+++ d7eeaac4-25be90ad8a27fce074de.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[617],{9447:function(t,a,c){"use strict";c.d(a,{mGl:function(){return r}});var e=c(5566);function r(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 1024 1024"},child:[{tag:"path",attr:{d:"M869 487.8L491.2 159.9c-2.9-2.5-6.6-3.9-10.5-3.9h-88.5c-7.4 0-10.8 9.2-5.2 14l350.2 304H152c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h585.1L386.9 854c-5.6 4.9-2.2 14 5.2 14h91.5c1.9 0 3.8-.7 5.2-2L869 536.2a32.07 32.07 0 0 0 0-48.4z"}}]})(t)}}}]);
+//# sourceMappingURL=d7eeaac4-25be90ad8a27fce074de.js.map
